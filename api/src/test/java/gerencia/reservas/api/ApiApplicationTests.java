0fbf0aebb7b3e57672b0bc14749badf09edeb767
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-10-24 11:08:46 -0300

    commit inicial

---
+++ api/src/test/java/gerencia/reservas/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gerencia.reservas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
