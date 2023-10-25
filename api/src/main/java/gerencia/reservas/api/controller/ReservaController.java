2928e464adfb4e48a13449391c115176a9305f2d
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-10-25 14:43:45 -0300

    adicionado os métodos listar e excluir em ReservaController

--- api/src/main/java/gerencia/reservas/api/controller/ReservaController.java
+++ api/src/main/java/gerencia/reservas/api/controller/ReservaController.java
@@ -2,6 +2,7 @@ package gerencia.reservas.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,12 +53,29 @@ public class ReservaController {
 	@Transactional
 	public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroReserva dados, UriComponentsBuilder uriBuilder) {
 		System.out.println("** CADASTRAR RESERVA ** ");
-		
 		var reserva = new Reserva(dados);
 		repository.save(reserva);
 		System.out.println("id reserva cadastrado:" + reserva.getId());
 		var uri = uriBuilder.path("/reserva/{id}").buildAndExpand(reserva.getId()).toUri();
 		return ResponseEntity.created(uri).body(new DadosDetalhamentoReserva(reserva));
+	}
+	
+	@GetMapping()
+	public ResponseEntity listar() {
+		System.out.println("** LISTAR RESERVAS ** ");
+		var page = repository.findAll();
+		return ResponseEntity.ok(page);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity excluir(@PathVariable Long id) {
+		System.out.println("** EXCLUIR RESERVA ** ");
+		if (!repository.existsById(id)) {
+			return ResponseEntity.badRequest().body("Id de reserva inexistente");
+		}
+		repository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
