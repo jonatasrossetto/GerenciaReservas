6506911e5710c36cc5a1b412201213745922e4f3
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-10-25 08:49:18 -0300

    adicionado métodos excluir e listar em HospedeController

--- api/src/main/java/gerencia/reservas/api/controller/HospedeController.java
+++ api/src/main/java/gerencia/reservas/api/controller/HospedeController.java
@@ -2,6 +2,7 @@ package gerencia.reservas.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,4 +57,23 @@ public class HospedeController {
 		return ResponseEntity.created(uri).body(new DadosDetalhamentoHospede(hospede));
 	}
 
+	@GetMapping()
+	public ResponseEntity listar() {
+		System.out.println("** LISTAR HÓSPEDES ** ");
+						
+		var page = repository.findAll();
+		return ResponseEntity.ok(page);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity excluir(@PathVariable Long id) {
+		System.out.println("** EXCLUIR HOSPEDE ** ");
+		if (!repository.existsById(id)) {
+			return ResponseEntity.badRequest().body("Id de hospede inexistente");
+		}
+		repository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
