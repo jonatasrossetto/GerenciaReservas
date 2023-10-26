c59b9ce2e80c6a95b380f9b4e2493c37284b415b
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-10-26 16:46:46 -0300

    adicionado inicialização em ApiApplication para ajustar a timezone para BRL, corrigindo a diferença entre data enviada e data recebida em ReservaController

--- api/src/main/java/gerencia/reservas/api/ApiApplication.java
+++ api/src/main/java/gerencia/reservas/api/ApiApplication.java
@@ -1,11 +1,22 @@
 package gerencia.reservas.api;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ApiApplication {
 
+	@PostConstruct
+	public void init(){
+	// Setting Spring Boot SetTimeZone
+	//para corrigir a diferença entre a data incorporada pelo controller e a data efetivamente enviada
+	TimeZone.setDefault(TimeZone.getTimeZone("BRT"));
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
