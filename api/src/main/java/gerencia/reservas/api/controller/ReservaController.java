92d14fb456aa08fb0c883b91744b4530cb31a667
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-10-26 15:58:06 -0300

    adicionado lógica para validar choque de datas em cadastrar em ReservaController

--- api/src/main/java/gerencia/reservas/api/controller/ReservaController.java
+++ api/src/main/java/gerencia/reservas/api/controller/ReservaController.java
@@ -28,29 +28,29 @@ public class ReservaController {
 
 	@Autowired
 	private ReservaRepository repository;
-	
+
 	@Autowired
 	private HospedeRepository hospedeRepository;
-	
+
 	@Autowired
 	private AcomodacaoRepository acomodacaoRepository;
-	
+
 	@GetMapping("/hello")
 	public String HelloWorld() {
 		return "O endpoint /reserva está funcionando";
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity detalhar(@PathVariable Long id) {
 		System.out.println("** DETALHAR RESERVA ** ");
 
-		//verifica se existe o id informado
+		// verifica se existe o id informado
 		if (!repository.existsById(id)) {
 			return ResponseEntity.badRequest().body("Id de reserva inexistente");
 		}
-				
+
 		Reserva reserva = repository.getReferenceById(id);
-		
+
 		return ResponseEntity.ok(new DadosDetalhamentoReserva(reserva));
 	}
 
@@ -58,6 +58,11 @@ public class ReservaController {
 	@Transactional
 	public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroReserva dados, UriComponentsBuilder uriBuilder) {
 		System.out.println("** CADASTRAR RESERVA ** ");
+		System.out.println("entrada: "+dados.dataEntrada());
+		System.out.println("saida: "+dados.dataSaida());
+		if (dados.dataEntrada().equals(dados.dataSaida())) {
+			return ResponseEntity.badRequest().body("A data de entrada e de saída são iguais");
+		}
 		if (!hospedeRepository.existsById(dados.hospedeId())) {
 			return ResponseEntity.badRequest().body("O hóspede informado não existe");
 		}
@@ -68,31 +73,36 @@ public class ReservaController {
 		if (acomodacao.getCapacidadePessoas()<dados.quantidadePessoas()) {
 			return ResponseEntity.badRequest().body("Quantidade de pessoas maior que a capacidade da acomodação");
 		}
-		
-		
-		//*verifica nas reservas para a acomodacaoId se a dataEntrada ou dataSaida informadas 
-		//estão dentro do período de alguma reserva já existente
+		var listaReservas = repository.findByAcomodacaoId(dados.acomodacaoId());
+		for (Reserva elemento : listaReservas) {
+			var conflitoDataEntrada = (dados.dataEntrada().after(elemento.getDataEntrada())&&dados.dataEntrada().before(elemento.getDataSaída()))||dados.dataEntrada().equals(elemento.getDataEntrada());
+			var conflitoDataSaida = dados.dataSaida().after(elemento.getDataEntrada())&&dados.dataSaida().before(elemento.getDataSaída());
+			if (conflitoDataEntrada||conflitoDataSaida) {
+				return ResponseEntity.badRequest().body("A acomodação já possui uma reserva dentro do período informado.");
+			}
+			
+		}
 		var reserva = new Reserva(dados);
 		repository.save(reserva);
 		System.out.println("id reserva cadastrado:" + reserva.getId());
 		var uri = uriBuilder.path("/reserva/{id}").buildAndExpand(reserva.getId()).toUri();
 		return ResponseEntity.created(uri).body(new DadosDetalhamentoReserva(reserva));
 	}
-	
+
 	@GetMapping()
 	public ResponseEntity listar() {
 		System.out.println("** LISTAR RESERVAS ** ");
 		var page = repository.findAll();
 		return ResponseEntity.ok(page);
 	}
-	
+
 	@GetMapping("/listarPorAcomodacao/{id}")
 	public ResponseEntity listarPorAcomodacao(@PathVariable Long id) {
 		System.out.println("** LISTAR RESERVAS POR ID DA ACOMODAÇÃO ** ");
 		var page = repository.findByAcomodacaoId(id);
 		return ResponseEntity.ok(page);
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@Transactional
 	public ResponseEntity excluir(@PathVariable Long id) {
@@ -103,20 +113,20 @@ public class ReservaController {
 		repository.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@PutMapping
 	@Transactional
 	public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoReserva dados) {
 		System.out.println("** ATUALIZAR RESERVA ** ");
-		
+
 		if (!repository.existsById(dados.id())) {
 			return ResponseEntity.badRequest().body("Id de reserva inexistente");
 		}
-		
+
 		var reserva = repository.getReferenceById(dados.id());
-		
+
 		reserva.atualizarInformacoes(dados);
-		
+
 		return ResponseEntity.ok(new DadosDetalhamentoReserva(reserva));
 	}
 
