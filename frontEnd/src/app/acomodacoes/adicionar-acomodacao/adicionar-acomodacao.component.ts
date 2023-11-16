c4287b982e6bd291ca3f4e7aa3bfd0ab53363605
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-11-16 13:24:55 -0300

    feat: Tela para cadastrar uma nova acomodação

--- frontEnd/src/app/acomodacoes/adicionar-acomodacao/adicionar-acomodacao.component.ts
+++ frontEnd/src/app/acomodacoes/adicionar-acomodacao/adicionar-acomodacao.component.ts
@@ -13,12 +13,53 @@ export class AdicionarAcomodacaoComponent {
   ) {}
 
   numero: number = 0;
+  capacidade: number = 0;
+  camas: number = 0;
+  valorDiaria: number = 0;
 
   cadastrarAcomodacao() {
     console.log('cadastrar acomodacao');
+    let acomodacao = {
+      numero: this.numero,
+      capacidadePessoas: this.capacidade,
+      quantidadeCamas: this.camas,
+      valorDiaria: this.valorDiaria,
+    };
+    console.log(acomodacao);
+    fetch('http://localhost:8080/acomodacao', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + sessionStorage.getItem('accessToken'),
+      },
+      body: JSON.stringify(acomodacao),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return { error: response.status };
+        }
+        return response.json();
+      })
+      .then((response) => {
+        if (response.error) {
+          console.log('Error: ' + response.error);
+          return;
+        } else {
+          console.log('Success: ' + response);
+          console.log('Acomodação cadastrada com sucesso!');
+          this._router.navigate(['gestao-acomodacao']);
+        }
+      })
+      .catch((e) => {
+        console.log('Error:' + e);
+      });
   }
 
   goDashBoard() {
     this._router.navigate(['dashboard']);
+  }
+  goGestaoAcomodacao() {
+    this._router.navigate(['gestao-acomodacao']);
   }
 }
