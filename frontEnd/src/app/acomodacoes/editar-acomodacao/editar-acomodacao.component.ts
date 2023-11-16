3bebe16b91a0caa1db5f11fe1c0a7c46e681965b
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-11-16 13:26:20 -0300

    feat: Tela para editar os dados de uma acomodação existente

--- frontEnd/src/app/acomodacoes/editar-acomodacao/editar-acomodacao.component.ts
+++ frontEnd/src/app/acomodacoes/editar-acomodacao/editar-acomodacao.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-acomodacao',
   templateUrl: './editar-acomodacao.component.html',
-  styleUrls: ['./editar-acomodacao.component.css']
+  styleUrls: ['./editar-acomodacao.component.css'],
 })
-export class EditarAcomodacaoComponent {
+export class EditarAcomodacaoComponent implements OnInit {
+  constructor(private _router: Router, private route: ActivatedRoute) {}
+  id: number = 0;
+  acomodacao = {
+    id: -1,
+    numero: -1,
+    capacidadePessoas: -1,
+    quantidadeCamas: -1,
+    valorDiaria: -1,
+  };
 
+  atualizarAcomodacao() {
+    console.log('atualizar acomodacao');
+    console.log(this.acomodacao);
+    fetch('http://localhost:8080/acomodacao', {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + sessionStorage.getItem('accessToken'),
+      },
+      body: JSON.stringify(this.acomodacao),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return { error: response.status };
+        }
+        return response.json();
+      })
+      .then((response) => {
+        if (response.error) {
+          console.log('Error: ' + response.error);
+          return;
+        } else {
+          console.log('Acomodação atualizada com sucesso!');
+          this.goGestaoAcomodacao();
+        }
+      })
+      .catch((e) => {
+        console.log('Error:' + e);
+      });
+  }
+  goDashBoard() {
+    this._router.navigate(['dashboard']);
+  }
+  goGestaoAcomodacao() {
+    this._router.navigate(['gestao-acomodacao']);
+  }
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.id = params['id'];
+      console.log(this.id);
+    });
+    fetch('http://localhost:8080/acomodacao/' + this.id, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + sessionStorage.getItem('accessToken'),
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return { error: response.status };
+        }
+        return response.json();
+      })
+      .then((response) => {
+        if (response.error) {
+          console.log('Error: ' + response.error);
+          return;
+        } else {
+          this.acomodacao = response;
+          this.acomodacao.id = this.id;
+          console.log(JSON.stringify(this.acomodacao));
+          console.log('Dados da acomodação recuperados com sucesso!');
+        }
+      })
+      .catch((e) => {
+        console.log('Error:' + e);
+      });
+  }
 }
